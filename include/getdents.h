2791fba340671acdb8f9ac7f7ae96d5117231272
Ramon Martins <ramonmartins.campos@gmail.com>
2024-02-06 18:22:05 -0300

    Create getdents.h

---
+++ include/getdents.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/syscalls.h>
+#include <linux/kallsyms.h>
+#include <linux/slab.h>
+#include <linux/kern_levels.h>
+#include <linux/gfp.h>
+#include <asm/unistd.h>
+#include <asm/paravirt.h>
+#include <linux/kernel.h>
+
+// Set up hooks.
+int SetHooks(void);
+void HookCleanup(void);
